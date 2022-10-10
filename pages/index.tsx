08c107afce3b3eee43422a0ccc6f04d407ec3be0
HTMLhead <mrdoo309@gmail.com>
2022-10-10 13:23:33 +0900

    :sparkles: app, document 컴포넌트 수정한 바에 맞게 업데이트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,36 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../interfaces/post'
+import Head from "next/head";
+// Component
+import Post from "../interfaces/post";
+// lib
+import { getAllPosts } from "../lib/api";
+import Meta from "../lib/Meta";
 
 type Props = {
-  allPosts: Post[]
-}
+  allPosts: Post[];
+};
 
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
   return (
     <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
+      <Meta />
+      <ul>
+        {allPosts.map((post) => (
+          <li key={post.slug}>
+            <a href={`posts/${post.slug}`}>
+              <div>{post.title}</div>
+            </a>
+            <div>{post.description}</div>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
+  const allPosts = getAllPosts(["title", "date", "category", "description", "tags", "slug"]);
 
   return {
     props: { allPosts },
-  }
-}
+  };
+};
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,75 +1,42 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import Container from '../../components/container'
-import PostBody from '../../components/post-body'
-import Header from '../../components/header'
-import PostHeader from '../../components/post-header'
-import Layout from '../../components/layout'
-import { getPostBySlug, getAllPosts } from '../../lib/api'
-import PostTitle from '../../components/post-title'
-import Head from 'next/head'
-import { CMS_NAME } from '../../lib/constants'
-import markdownToHtml from '../../lib/markdownToHtml'
-import type PostType from '../../interfaces/post'
+import { useRouter } from "next/router";
+import { getPostBySlug, getAllPosts } from "../../lib/api";
+import Head from "next/head";
+import markdownToHtml from "../../lib/markdownToHtml";
+import Meta from "../../lib/Meta";
+
+import type PostType from "../../interfaces/post";
 
 type Props = {
-  post: PostType
-  morePosts: PostType[]
-  preview?: boolean
-}
+  post: PostType;
+};
 
-export default function Post({ post, morePosts, preview }: Props) {
-  const router = useRouter()
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
-  }
-  return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-          </>
-        )}
-      </Container>
-    </Layout>
-  )
+export default function Post({ post }: Props) {
+  const router = useRouter();
+
+  return router.isFallback ? (
+    <div>loading</div>
+  ) : (
+    <>
+      <Meta />
+      <Head>
+        <title>{post.title} | Head with nextjs</title>
+      </Head>
+      <div>{post.description}</div>
+      <div dangerouslySetInnerHTML={{ __html: post.content }} />
+    </>
+  );
 }
 
 type Params = {
   params: {
-    slug: string
-  }
-}
+    slug: string;
+  };
+};
 
 export async function getStaticProps({ params }: Params) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ])
-  const content = await markdownToHtml(post.content || '')
+  const post = getPostBySlug(params.slug, ["title", "date", "slug", "description", "content"]);
+
+  const content = await markdownToHtml(post.content || "");
 
   return {
     props: {
@@ -78,11 +45,11 @@ export async function getStaticProps({ params }: Params) {
         content,
       },
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(['slug'])
+  const posts = getAllPosts(["slug"]);
 
   return {
     paths: posts.map((post) => {
@@ -90,8 +57,8 @@ export async function getStaticPaths() {
         params: {
           slug: post.slug,
         },
-      }
+      };
     }),
     fallback: false,
-  }
+  };
 }
